cbb3d59638653340bb3ebc8b447b446d7a3c9e0b
Marek Štefkovič <marek@stefkovic.com>
2018-11-07 22:14:17 +0100

    Init with MQTT demo

---
+++ src/config_template.h
@@ -0,0 +1,9 @@
+// Rename this file to config.h and change accordingly
+
+// WiFi
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+// MQTT
+#define MQTT_HOST IPAddress(192, 168, 1, 2)
+#define MQTT_PORT 1883
