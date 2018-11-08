935c65598d215f05a4a2ebd84db1c5b226c75429
Marek Štefkovič <marek@stefkovic.com>
2018-11-08 10:03:25 +0100

    Added DHT sensor

--- src/config_template.h
+++ src/config_template.h
@@ -1,5 +1,7 @@
 // Rename this file to config.h and change accordingly
 
+#include <Arduino.h>
+
 // WiFi
 #define WIFI_SSID ""
 #define WIFI_PASS ""
@@ -7,3 +9,6 @@
 // MQTT
 #define MQTT_HOST IPAddress(192, 168, 1, 2)
 #define MQTT_PORT 1883
+
+// SENSORS
+#define SENSOR_DHT_PIN D1
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Ticker.h>
 #include <ESP8266WiFi.h>
 #include <AsyncMqttClient.h>
+#include <DHT.h>
 
 AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
@@ -10,6 +11,9 @@ Ticker mqttReconnectTimer;
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
+
+DHT dht;
+Ticker dhtTimer;
 
 void connectToWifi() {
   Serial.println("Connecting to WiFi...");
@@ -37,17 +41,6 @@ void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT.");
   Serial.print("Session present: ");
   Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 1);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
-  mqttClient.publish("test/lol", 0, true, "test 1");
-  Serial.println("Publishing at QoS 0");
-  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
-  Serial.print("Publishing at QoS 1, packetId: ");
-  Serial.println(packetIdPub1);
-  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
-  Serial.print("Publishing at QoS 2, packetId: ");
-  Serial.println(packetIdPub2);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
@@ -96,8 +89,31 @@ void onMqttPublish(uint16_t packetId) {
   Serial.println(packetId);
 }
 
+void readDHT() {
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print("°C, Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  char temp[] = "";
+  dtostrf(temperature, 3, 2, temp);
+  mqttClient.publish("sensor/temperature", 1, true, temp);
+  char hum[] = "";
+  dtostrf(humidity, 3, 2, hum);
+  mqttClient.publish("sensor/humidity", 1, true, hum);
+}
+
+void setupSensors() {
+  dht.setup(D1);
+  dhtTimer.attach(5, readDHT);
+}
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
   wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
@@ -111,6 +127,7 @@ void setup() {
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 
   connectToWifi();
+  setupSensors();
 }
 
 void loop() {
